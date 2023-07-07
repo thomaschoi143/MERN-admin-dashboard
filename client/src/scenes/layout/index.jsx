83a61933026eec516b0b835917f6329396405491
thomaschoi143 <thomasa143a@gmail.com>
2023-07-07 22:53:36 +1000

    Implement Products, Customers, Transactions

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+
+const Layout = () => {
+	const isNonMobile = useMediaQuery("(min-width: 600px)");
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+	const { userId } = useSelector((state) => state.global);
+	const { data } = useGetUserQuery(userId);
+
+	return (
+		<Box width="100%" height="100%" display={isNonMobile ? "flex" : "block"}>
+			<Sidebar
+				user={data || []}
+				isNonMobile={isNonMobile}
+				drawerWidth="250px"
+				isSidebarOpen={isSidebarOpen}
+				setIsSidebarOpen={setIsSidebarOpen}
+			/>
+			<Box flexGrow={1}>
+				<Navbar
+					user={data || []}
+					isSidebarOpen={isSidebarOpen}
+					setIsSidebarOpen={setIsSidebarOpen}
+				/>
+				<Outlet />
+			</Box>
+		</Box>
+	);
+};
+
+export default Layout;
